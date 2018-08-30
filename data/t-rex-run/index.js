7db3305a713b1c0d2de6711cb883e378e3800275
zhouyg <haraldred@163.com>
2018-08-30 11:52:43 +0800

    hello ai

--- data/t-rex-run/index.js
+++ data/t-rex-run/index.js
@@ -7,21 +7,21 @@ var ii = 0;
 
 // tf code
 // nn - 2
-const ni = 20;
-const rate = 1;
+const ni = 50;
+const rate = 0.1;
 
 const adamOptimizer = tf.train.adam(rate);
 
-const w1 = tf.variable(tf.tensor2d([0, 0, 0],[3,1]));
+const w1 = tf.variable(tf.zeros([3, 1]));
 const b1 = tf.variable(tf.scalar(0));
 
-const w2 = tf.variable(tf.scalar(0));
+const w2 = tf.variable(tf.zeros([1, 1]));
 const b2 = tf.variable(tf.scalar(0));
 
 //线性公式 y = w * x + b
 const f1 = (x) => tf.sigmoid(x.matMul(w1).add(b1));
 
-const f2 = (x) => w2.mul(x).add(b2);
+const f2 = (x) => tf.sigmoid(x.matMul(w2).add(b2));
 
 // loss function
 function lossFn(y, yHat) {
@@ -48,9 +48,9 @@ const runner = new Runner('#game', {
   onCrash: () => {
     // fix result
     if (lastState.r >= critical) {
-      lastState.r = 1;
-    } else {
       lastState.r = 0;
+    } else {
+      lastState.r = 1;
     }
 
     trainSet.push(Object.assign({}, lastState));
@@ -59,11 +59,11 @@ const runner = new Runner('#game', {
 
     w1.print();
 
-    var trainY = (tf.tensor2d(trainSet.map(obj => obj.r), [trainSet.length, 1]));
+    var trainY = (tf.tensor2d(trainSet.map(obj => [obj.r])));
 
-    for (var i = 0; i < trainSet.length * 10; i++) {
+    for (var i = 0; i < 1; i++) {
       adamOptimizer.minimize(() => {
-        const r = lossFn(f1(trainX), trainY);
+        const r = lossFn(f2(f1(trainX)), trainY);
         return r;
       });
     }
@@ -77,20 +77,22 @@ const runner = new Runner('#game', {
     }
     const {obstacleX, obstacleWidth, speed} = state;
 
-    const x = tf.tensor2d([[obstacleX, obstacleWidth, speed]]);
-    const z1 = f1(x);
-    const z2 = z1.dataSync()[0];
+    const z2 = tf.tidy(() => {
+      const x = tf.tensor2d([[obstacleX, obstacleWidth, speed]]);
+      const z1 = f1(x);
+      const z2 = f2(z1);
+      return z2.dataSync()[0];
+    });
 
     lastState = {
       s: [obstacleX, obstacleWidth, speed],
       r: z2,
     };
 
-
     if (z2 >= critical) {
-      return Promise.resolve(0)
-    } else {
       return Promise.resolve(1)
+    } else {
+      return Promise.resolve(0)
     }
   },
 });
